e58d020f988eba5d998e74095f67eca295702940
fallupboy <svity21@gmail.com>
2020-12-08 18:46:33 +0200

    init

---
+++ BookStore/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+
+        // Show all books on page
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        // Add book to the DB
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        // Edit book in the DB
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var b = db.Books.Find(id);
+            return View(b);
+        }
+        [HttpPost]
+        public ActionResult Edit(Book b)
+        {   
+            db.Entry(b).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        // Buy book
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return RedirectToAction("BuySuccesful");
+        }
+
+        // Successful purchase
+        public ActionResult BuySuccesful()
+        {
+            return View();
+        }
+
+        // Show all the purchases that were made
+        public ActionResult Purchases()
+        {
+            IEnumerable<Purchase> purchases = db.Purchases;
+            ViewBag.Purchases = purchases;
+            return View();
+        }
+    }
+}
---
+++ BookStore/Models/BookDbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Game of Thrones", Author = "George R. R. Martin", Quantity = 5, Price = 45 });
+            db.Books.Add(new Book { Name = "Fifty Shades of Grey", Author = "James, E. L.", Quantity = 0, Price = 25 });
+            db.Books.Add(new Book { Name = "Harry Potter and the Deathly Hallows", Author = "Rowling, J.K.", Quantity = 2, Price = 30 });
+
+            base.Seed(db);
+        }
+    }
+}
